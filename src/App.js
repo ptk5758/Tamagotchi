b8a3ff3ed01d4bc9b08f0943614191958533b327
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2022-10-04 23:00:47 +0900

    박태광 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ParkTaeGwang from './ParkTaeGwang';
 function App() {
   return (
     <div>     
       이현승 hello worl
-
+      <ParkTaeGwang/>
      
     </div>
   );
---
+++ src/ParkTaeGwang.js
@@ -0,0 +1,7 @@
+function ParkTaeGwang() {
+    return(
+        <div>박태광 컴포넌트</div>
+    );
+}
+
+export default ParkTaeGwang
