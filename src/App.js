a0bb408110719646473ed75d80442ba9f6e96eca
CaKlan <lhs74187@gmail.com>
2022-10-05 22:58:55 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState , useRef} from 'react';
 import './App.css';
 
 function App() {
   let [dama, setDama] = useState({
     stamina : 0
   });
+  
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    useEffect(()=>{
+      savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(()=>{
+      function tick() {
+        savedCallback.current();
+      }
+      if(delay != null){
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay])
+  }
+
+  useInterval(() => {
+    setDama(Object.assign({},dama).stamina + 1);
+  })
+  
+
   return (
     <div className='content'>
       <main>
@@ -14,9 +38,10 @@ function App() {
         <div className='action-group'>
           <span className='action-item'>밥주기</span>
           <span className='action-item' onClick={()=>{
-            let temp = dama
+            let temp = Object.assign({}, dama);
             temp.stamina += 10;
             setDama(temp);
+            console.log(dama);
           }}>놀아주기</span>
           <span className='action-item'>씻겨주기</span>
         </div>
@@ -24,5 +49,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
